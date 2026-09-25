aa9e599fb6e8de93da948e9bc5210d38957740fa
casimsimanchristian5-create <casimsimanchristian5@gmail.com>
2026-09-25 10:43:25 +0000

    Update Task Manager

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'task_name' => 'required',
+            'description' => 'nullable',
+            'status' => 'required',
+            'due_date' => 'nullable|date',
+        ]);
+
+        Task::create($request->all());
+
+        return redirect()->to(config('app.url'));
+    }
+
+    public function edit($id)
+    {
+        $task = Task::findOrFail($id);
+
+        return view('tasks.edit', compact('task'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'task_name' => 'required',
+            'description' => 'nullable',
+            'status' => 'required',
+            'due_date' => 'nullable|date',
+        ]);
+
+        $task = Task::findOrFail($id);
+
+        $task->update($validated);
+
+        return redirect()->to(config('app.url'));
+    }
+
+    public function destroy($id)
+    {
+        Task::findOrFail($id)->delete();
+
+        return redirect()->to(config('app.url'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TaskController::class, 'index'])
+    ->name('tasks.index');
+
+Route::get('/tasks', [TaskController::class, 'index'])
+    ->name('tasks.list');
+
+Route::post('/tasks', [TaskController::class, 'store'])
+    ->name('tasks.store');
+
+// Edit Task
+Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])
+    ->name('tasks.edit');
+
+// Update Task
+Route::put('/tasks/{id}', [TaskController::class, 'update'])
+    ->name('tasks.update');
+
+// Delete Task
+Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])
+    ->name('tasks.destroy');
